fc0af41b8bf1adcf56621f4fab2e9395d4679a0f
Josh Henry <josh.d.henry@gmail.com>
2016-10-29 04:17:27 -0700

    Moved several functions from ViewController to LocationAPIService class.

--- RainOrShine/LocationAPIService.swift
+++ RainOrShine/LocationAPIService.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 import GooglePlaces
+import SwiftyJSON
 
 class LocationAPIService {
+    var keys: NSDictionary = NSDictionary()
+
     var placesClient: GMSPlacesClient? = GMSPlacesClient.shared()
     var currentPlace: GMSPlace?
+    
+    var firstGeneralLocalePhotoMetaData: GMSPlacePhotoMetadata?
+    var firstGeneralLocalePhoto: UIImage?
+
+    
+    func setAPIKeys() {
+        guard let path = Bundle.main.path(forResource: "APIKeys", ofType: "plist") else {return}
+        keys = NSDictionary(contentsOfFile: path)!
+    }
     
     
     func getCurrentLocation(completion: @escaping (_ result: Bool)->()) {
@@ -31,7 +43,167 @@ class LocationAPIService {
                 completion(true)
             }
         })
-        if placeFindComplete == false {
+        if (placeFindComplete == false) {
+            completion(false)
+        }
+    }
+    
+    
+    func setPhotoOfGeneralLocale(size: CGSize, scale: CGFloat, completion: @escaping (_ result: Bool) ->()) {
+        print("In function getPhotoOfGeneralLocale...")
+
+        //let setPhotoComplete: Bool = false
+        let generalLocaleString: String = getGeneralLocaleString()
+        
+        //Get the place ID of the general area so that we can grab an image of the city
+        let placeIDOfGeneralArea: String? = getPlaceIDOfGeneralLocale(generalLocaleQueryString: generalLocaleString)
+        if (placeIDOfGeneralArea != nil) {
+            setPhotoMetaDataForCurrentLocation() { (photoMetaDataFound) -> () in
+                if (photoMetaDataFound == true) {
+                    self.setImageForMetadata(size: size, scale: scale) { (imageFound) -> () in
+                        
+                        if (imageFound == true) {
+                            completion(true)
+                        }
+                        else {
+                            completion(false)
+                        }
+                    }
+                }
+            }
+            /*if (setPhotoComplete == false) {
+                completion(false)
+            }*/
+        }
+        else {
+            print("Not loading a photo since place ID of general area was nil...")
+            completion(true)
+        }
+    }
+    
+    
+    //This method builds a string of the general locality of the place
+    func getGeneralLocaleString() -> String {
+        print("In function getGeneralLocaleString...")
+
+        var queryString: String = String()
+        
+        for addressComponent in (currentPlace?.addressComponents)! {
+            print(addressComponent.type)
+            print(addressComponent.name)
+            
+            switch (addressComponent.type) {
+                //case "sublocality_level_1":
+            //    queryString += thisType.name
+            case "locality":
+                queryString += addressComponent.name
+            case "administrative_area_level_1":
+                queryString += "+" + addressComponent.name
+            case "country":
+                queryString += "+" + addressComponent.name
+            default:
+                break
+            }
+        }
+        
+        //Replace any spaces in the URL with "+"
+        queryString = queryString.replacingOccurrences(of: " ", with: "+")
+        
+        return queryString
+    }
+    
+    
+    //This method takes a general area string (such as "Atlanta, Georgia, United States") and gets a place ID for that area
+    func getPlaceIDOfGeneralLocale(generalLocaleQueryString: String) -> String? {
+        print("In function getPlaceIDOfGeneralLocale...")
+
+        var placeID: String?
+        var completionHandlerCodeComplete: Bool = false
+        
+        let placeTextSearchURL: String = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + generalLocaleQueryString + "&key=" + (keys["GooglePlacesAPIKeyWeb"] as! String)
+        print("placeTextSearchURL is \(placeTextSearchURL)")
+        
+        let session = URLSession.shared
+        /*guard let url = URL(string: placeTextSearchURL) else {
+         print("OH NOOOO")
+         return nil
+         }*/
+        //I NEED TO CHECK HERE THAT THERE IS NO ERROR.
+        //PREVIOUSLY IT CRASHED IF THERE WERE SPACES IN THE URL.  I NEED TO FIND OUT HOW ELSE IT COULD CRASH
+        let url = URL(string: placeTextSearchURL)!
+        
+        // Make call. Handle it in a completion handler.
+        session.dataTask(with: url as URL, completionHandler: { ( data: Data?, response: URLResponse?, error: Error?) -> Void in
+            //Ensure the  response isn't an error
+            guard let thisURLResponse = response as? HTTPURLResponse,
+                thisURLResponse.statusCode == 200 else {
+                    print("Not a 200 (successful) response")
+                    return
+            }
+            let json = JSON(data: data!)
+            placeID = json["results"][0]["place_id"].string
+            
+            completionHandlerCodeComplete = true
+        }).resume()
+        
+        while (completionHandlerCodeComplete == false) {
+            print("Waiting on the photo reference to retrieve...")
+        }
+        return placeID
+    }
+    
+    
+    //Retrieve photo metadata for place
+    func setPhotoMetaDataForCurrentLocation(completion: @escaping (_ result: Bool)->()) {
+        print("In function setPhotoMetaDataForCurrentLocation...")
+
+        var photoMetaDataFindComplete: Bool = false
+        
+        GMSPlacesClient.shared().lookUpPhotos(forPlaceID: (currentPlace?.placeID)!) { (photos, error) -> Void in
+            if let error = error {
+                print("Error loading photo from Google API: \(error.localizedDescription)")
+                completion(true)
+                return
+            } else {
+                if let firstPhotoMetadata = photos?.results.first {
+                    self.firstGeneralLocalePhotoMetaData = firstPhotoMetadata
+                    photoMetaDataFindComplete = true
+                    completion(true)
+                }
+                else {
+                    print("No photos found. Resetting image view to blank...")
+                    self.firstGeneralLocalePhotoMetaData = nil
+                    completion(true)
+                }
+            }
+        }
+        if (photoMetaDataFindComplete == false) {
+            completion(false)
+        }
+    }
+    
+    
+    //Retrieve image based on place metadata
+    func setImageForMetadata(size: CGSize, scale: CGFloat, completion: @escaping (_ result: Bool) ->()) {
+        print("In function setImageForMetadata...")
+
+        var imageFindComplete: Bool = false
+        
+        GMSPlacesClient.shared().loadPlacePhoto(firstGeneralLocalePhotoMetaData!, constrainedTo: size, scale: scale) { (photo, error) -> Void in
+            if let error = error {
+                print("Error loading image for metadata: \(error.localizedDescription)")
+                completion(true)
+                return
+            } else {
+                //self.locationImageView.image = photo
+                //self.attributionTextView.attributedText = photoMetadata.attributions
+                self.firstGeneralLocalePhoto = photo
+                print("self.firstGeneralLocalePhoto is \(self.firstGeneralLocalePhoto)")
+                imageFindComplete = true
+                completion(true)
+            }
+        }
+        if (imageFindComplete == false) {
             completion(false)
         }
     }
--- RainOrShine/ViewController.swift
+++ RainOrShine/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var locationImageView: UIImageView!
  
-    var keys: NSDictionary = NSDictionary()
     let locationManager = CLLocationManager()
     
     var placesClient: GMSPlacesClient?
@@ -34,18 +33,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         placesClient = GMSPlacesClient.shared()
-        
-        getAPIKeys()
-
         locationAPIService = LocationAPIService()
-        
+        locationAPIService?.setAPIKeys()
         displayLocationSearchBar()
-    }
-    
-    
-    func getAPIKeys() {
-        guard let path = Bundle.main.path(forResource: "APIKeys", ofType: "plist") else {return}
-        keys = NSDictionary(contentsOfFile: path)!
     }
     
     
@@ -71,14 +61,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let screenWidth = UIScreen.main.bounds.width
         
         let subView = UIView(frame: CGRect(x: 0, y: 20, width: screenWidth, height: 45))
-        
         subView.addSubview((searchController?.searchBar)!)
         self.view.addSubview(subView)
         searchController?.searchBar.sizeToFit()
-        //searchController?.hidesNavigationBarDuringPresentation = false
         
-        // When UISearchController presents the results view, present it in
-        // this view controller, not one further up the chain.
+        //When UISearchController presents the results view, present it in, this view controller, not one further up the chain.
         self.definesPresentationContext = true
     }
     
@@ -95,125 +82,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 //https://developers.google.com/places/ios-api/attributions
                 //LINKS IN ATTRIBUTIONS MUST BE TAPPABLE
                 
-                print("coords \(place?.coordinate)")
-                let cityQueryString = self.getCityQueryString(place: place)
-                
-                //Get the place ID of the general area so that we can grab an image of the city
-                let placeIDOfGeneralArea: String? = self.getPlaceIDOfGeneralArea(cityQuery: cityQueryString)
-                
-                if (placeIDOfGeneralArea != nil) {
-                    self.loadFirstPhotoForPlace(placeID: placeIDOfGeneralArea!)
-                }
-                else {
-                    print("Not loading a photo since place ID of general area was nil...")
-                    self.resetLocationImageView()
+                self.locationAPIService?.setPhotoOfGeneralLocale(size: self.locationImageView.bounds.size, scale: self.locationImageView.window!.screen.scale) { (imageSet) -> () in
+                    self.locationImageView.image = self.locationAPIService?.firstGeneralLocalePhoto
+                    print(self.locationImageView.image)
                 }
             }
         }
     }
-    
-    
-    //This method builds a string of the general locality of the place
-    func getCityQueryString(place: GMSPlace?) -> String {
-        var queryString: String = String()
-        
-        for addressComponent in (place?.addressComponents)! {
-            print(addressComponent.type)
-            print(addressComponent.name)
-            
-            switch (addressComponent.type) {
-                //case "sublocality_level_1":
-            //    queryString += thisType.name
-            case "locality":
-                queryString += addressComponent.name
-            case "administrative_area_level_1":
-                queryString += "+" + addressComponent.name
-            case "country":
-                queryString += "+" + addressComponent.name
-            default:
-                break
-            }
-        }
-        
-        //Replace any spaces in the URL with "+"
-        queryString = queryString.replacingOccurrences(of: " ", with: "+")
-        
-        return queryString
-    }
-  
-    
-    //This method takes a general area string (such as "Atlanta, Georgia, United States") and gets a place ID for that area
-    func getPlaceIDOfGeneralArea(cityQuery: String) -> String? {
-        var placeID: String?
-        var completionHandlerCodeComplete: Bool = false
-        
-        print(cityQuery)
-        
-        let placeTextSearchURL: String = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + cityQuery + "&key=" + (keys["GooglePlacesAPIKeyWeb"] as! String)
-
-        
-        print("placeTextSearchURL is \(placeTextSearchURL)")
-        
-        let session = URLSession.shared
-        /*guard let url = URL(string: placeTextSearchURL) else {
-            print("OH NOOOO")
-            return nil
-        }*/
-        //I NEED TO CHECK HERE THAT THERE IS NO ERROR.
-        //PREVIOUSLY IT CRASHED IF THERE WERE SPACES IN THE URL.  I NEED TO FIND OUT HOW ELSE IT COULD CRASH
-        let url = URL(string: placeTextSearchURL)!
-        
-        // Make call. Handle it in a completion handler.
-        session.dataTask(with: url as URL, completionHandler: { ( data: Data?, response: URLResponse?, error: Error?) -> Void in
-            //Ensure the  response isn't an error
-            guard let thisURLResponse = response as? HTTPURLResponse,
-                thisURLResponse.statusCode == 200 else {
-                    print("Not a 200 (successful) response")
-                    return
-            }
-            let json = JSON(data: data!)
-            placeID = json["results"][0]["place_id"].string
-            
-            completionHandlerCodeComplete = true
-        }).resume()
-        
-        while (completionHandlerCodeComplete == false) {
-            print("Waiting on the photo reference to retrieve...")
-        }
-        return placeID
-    }
-    
-    
-    //Retrieve photo metadata for place
-    func loadFirstPhotoForPlace(placeID: String) {
-        GMSPlacesClient.shared().lookUpPhotos(forPlaceID: placeID) { (photos, error) -> Void in
-            if let error = error {
-                print("Error loading photo from Google API: \(error.localizedDescription)")
-            } else {
-                if let firstPhoto = photos?.results.first {
-                    self.loadImageForMetadata(photoMetadata: firstPhoto)
-                }
-                else {
-                    print("No photos found. Resetting image view to blank...")
-                    self.resetLocationImageView()
-                }
-            }
-        }
-    }
-    
-    
-    //Retrieve image based on place metadata
-    func loadImageForMetadata(photoMetadata: GMSPlacePhotoMetadata) {
-        GMSPlacesClient.shared().loadPlacePhoto(photoMetadata, constrainedTo: locationImageView.bounds.size, scale: self.locationImageView.window!.screen.scale) { (photo, error) -> Void in
-            if let error = error {
-                print("Error loading image for metadata: \(error.localizedDescription)")
-            } else {
-                self.locationImageView.image = photo
-                //self.attributionTextView.attributedText = photoMetadata.attributions;
-            }
-        }
-    }
-    
     
     func resetLocationImageView() {
         self.locationImageView.image = nil
