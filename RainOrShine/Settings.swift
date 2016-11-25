82a2ad4d490ced3b352ec5cc37b7dbd279da63ca
Josh Henry <josh.d.henry@gmail.com>
2016-11-25 02:44:39 -0800

    UI Improvements... Added more settings to Settings struct.

--- RainOrShine/Settings.swift
+++ RainOrShine/Settings.swift
@@ -18,8 +18,28 @@ struct Settings {
         never = "never"
     }
     
+    enum TemperatureUnitSetting: String {
+        case fahrenheit = "fahrenheit",
+        celcius = "celcius"
+    }
     
-    // MARK: - Methods
+    enum UpdateWeatherIntervalSetting: String {
+        case fifteen = "15",
+        thirty = "30",
+        sixty = "60"
+    }
+    
+    enum ChangePhotoIntervalSetting: String {
+        case one = "1",
+        three = "3",
+        five = "5",
+        ten = "10",
+        thirty = "30",
+        never = "0"
+    }
+    
+    
+    // MARK: Computed Properties
     var useDefaultPhotos: UseDefaultPhotosSetting {
         get {
             let appSettingRawValue: String = savedAppSettings.object(forKey: "useDefaultPhotos") as? String ?? UseDefaultPhotosSetting.whenNoPictures.rawValue
@@ -28,6 +48,64 @@ struct Settings {
         }
         set {
             savedAppSettings.set(newValue.rawValue, forKey: "useDefaultPhotos")
+        }
+    }
+    
+    
+    var temperatureUnit: TemperatureUnitSetting {
+        get {
+            let appSettingRawValue: String = savedAppSettings.object(forKey: "temperatureUnit") as? String ?? TemperatureUnitSetting.fahrenheit.rawValue
+            let temperatureUnitEnumValue: TemperatureUnitSetting = TemperatureUnitSetting(rawValue: appSettingRawValue) ?? TemperatureUnitSetting.fahrenheit
+            return temperatureUnitEnumValue
+        }
+        set {
+            savedAppSettings.set(newValue.rawValue, forKey: "temperatureUnit")
+        }
+    }
+    
+    
+    var updateWeatherInterval: UpdateWeatherIntervalSetting {
+        get {
+            let appSettingRawValue: String = savedAppSettings.object(forKey: "updateWeatherInterval") as? String ?? UpdateWeatherIntervalSetting.thirty.rawValue
+            let updateWeatherIntervalEnumValue: UpdateWeatherIntervalSetting = UpdateWeatherIntervalSetting(rawValue: appSettingRawValue) ?? UpdateWeatherIntervalSetting.thirty
+            return updateWeatherIntervalEnumValue
+        }
+        set {
+            savedAppSettings.set(newValue.rawValue, forKey: "updateWeatherInterval")
+        }
+    }
+    
+    
+    var changePhotoInterval: ChangePhotoIntervalSetting {
+        get {
+            let appSettingRawValue: String = savedAppSettings.object(forKey: "changePhotoInterval") as? String ?? ChangePhotoIntervalSetting.three.rawValue
+            let updateWeatherIntervalEnumValue: ChangePhotoIntervalSetting = ChangePhotoIntervalSetting(rawValue: appSettingRawValue) ?? ChangePhotoIntervalSetting.three
+            return updateWeatherIntervalEnumValue
+        }
+        set {
+            savedAppSettings.set(newValue.rawValue, forKey: "changePhotoInterval")
+        }
+    }
+    
+    
+    var nightStandModeOn: Bool {
+        get {
+            let nightStandModeAppSetting: Bool = savedAppSettings.object(forKey: "nightStandModeOn") as? Bool ?? false
+            return nightStandModeAppSetting
+        }
+        set {
+            savedAppSettings.set(newValue, forKey: "nightStandModeOn")
+        }
+    }
+    
+    
+    var removeAdsPurchased: Bool {
+        get {
+            let removeAdsPurchasedAppSetting: Bool = savedAppSettings.object(forKey: "removeAdsPurchased") as? Bool ?? false
+            return removeAdsPurchasedAppSetting
+        }
+        set {
+            savedAppSettings.set(newValue, forKey: "removeAdsPurchased")
         }
     }
 }
